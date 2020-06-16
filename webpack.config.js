52805c344da28a7b89861d774a7e7bd17746f4c3
Rachel Cherry <bamadesigner@gmail.com>
2020-06-16 13:54:16 -0400

    Moving footer CSS to footer component

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
 	entry: "./src/wpcampus-footer.js",
 	mode: "production",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "wpcampus-footer.min.js"
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [
+					{ loader: "style-loader" },
+					{ loader: MiniCssExtractPlugin.loader },
+					{ loader: "css-loader", options: { sourceMap: false } },
+					{ loader: "sass-loader", options: { sourceMap: false } }
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+	]
 };
